d2e619dae9b0de3efb55894bdf92738fd7a51137
Lanseeg <38041866+Lanseeg@users.noreply.github.com>
2024-08-31 03:38:42 +0200

    in modal.js the function modalWindow1 now calls adminGallery which use works already stored. deleteItem allows user to make a httpDelete (from utils.js) and delete the selected work. Deletion can be aborted if user click cancel

--- FrontEnd/script/modal.js
+++ FrontEnd/script/modal.js
@@ -1,7 +1,8 @@
 "use strict";
 
+// MODAL
 /**
- * CREATE MODAL & POPULATE
+ * CREATE MODAL & POPULATE IT
  * 
  * @param {Function} [populateModalFunction] - An optional function that populates the header, content, and footer of the modal.
  * @returns {Object} An object containing references to the modal, backdrop, header, content, and footer.
@@ -14,15 +15,15 @@ function createModal(populateModalFunction = null) {
         body.removeChild(existingModal);
     }
 
-    // Create the modal container
+    // Modal container
     let editModal = document.createElement('aside');
     editModal.id = 'edit-modal'; // Set the ID for the modal container
 
-    // Create the modal background
+    // Modal background
     let modalBackground = document.createElement('div');
     modalBackground.classList.add('modal-background'); // Add a background overlay for the modal
 
-    // Create the modal window structure (header, content, footer)
+    // Modal window structure (header, content, footer)
     let modalWindow = document.createElement('div');
     modalWindow.classList.add('modal-window');
 
@@ -51,37 +52,8 @@ function createModal(populateModalFunction = null) {
         modalWindow.style.opacity = '1';
     }, 10);
 
-    // Populate modal content with provided function (if any)
-    if (populateModalFunction) {
-        populateModalFunction(modalHeader, modalContent, modalFooter);
-    }
-
-    // Closing the modal
-    // Close on click outside the modal window
-    modalBackground.addEventListener('click', (e) => {
-        if (e.target === modalBackground) {
-            closeModal(editModal);
-            console.log("Closing modal clicking outside");
-        }
-    });
-
-    // Close on pressing Esc
-    window.addEventListener('keydown', (e) => {
-        if (e.key === "Escape" || e.key === "Esc") {
-            closeModal(editModal);
-            console.log("Closing modal pressing Esc.");
-        }
-    });
-
-    // Add event listener to the default close button
-    const closeBtn = document.querySelector('.close')
-    closeBtn.addEventListener('click', () => {
-        closeModal(editModal);
-        console.log("Closing modal clicking X");
-    });
-
     /**
-     * Close the modal with a fade-out effect.
+     * Close modal function with fade-out
      * 
      * @param {HTMLElement} modal - The modal element to be closed.
      */
@@ -98,97 +70,108 @@ function createModal(populateModalFunction = null) {
         }, 500);
     }
 
+    // Populate modal content with provided function (if any)
+    if (populateModalFunction) {
+        populateModalFunction(modalHeader, modalContent, modalFooter, closeModal);
+    }
+
+    // Closing the modal
+    // Close on click outside the modal window
+    modalBackground.addEventListener('click', (e) => {
+        if (e.target === modalBackground) {
+            closeModal(editModal);
+            console.log("Closing modal clicking outside");
+        }
+    });
+
+    // Close on pressing Esc. key
+    window.addEventListener('keydown', (e) => {
+        if (e.key === "Escape" || e.key === "Esc") {
+            closeModal(editModal);
+            console.log("Closing modal pressing Esc.");
+        }
+    });
+
     // Return references for future manipulation
     return { modal: editModal, backdrop: modalBackground, header: modalHeader, content: modalContent, footer: modalFooter };
 }
 
-// createModal example:
-// createModal(modalWindow1);
-// createModal(modalWindow1);
-
 /**
  * MODAL WINDOW 1 (admin gallery & httpDELETE from utils.js)
- * to be used as a parameter in createModal()
+ * To be used as a parameter in createModal()
  * 
- * @param {HTMLElement} header - The header element to be populated.
- * @param {HTMLElement} content - The content element to be populated.
- * @param {HTMLElement} footer - The footer element to be populated.
+ * @param {HTMLElement} header
+ * @param {HTMLElement} content
+ * @param {HTMLElement} footer
  */
-function modalWindow1(header, content, footer) {
+function modalWindow1(header, content, footer, closeModal) {
     // HEADER
     const backButton = document.createElement('button');
-    backButton.classList.add('back'); // Add class for the back button
-    backButton.title = 'Back'; // Set title attribute
-    backButton.style.display = 'none'; // Initially hide the back button
+    backButton.classList.add('back');
+    backButton.title = 'Back';
+    backButton.style.display = 'none'; // Hide the back button in window 1
     backButton.innerHTML = '<i class="fa-solid fa-arrow-left" id="modal-return"></i>'; // Set the back button icon
 
     const flexSpace = document.createElement('div');
-    flexSpace.classList.add('modal-flex-space'); // Add flexible space for header layout
+    flexSpace.classList.add('modal-flex-space');
 
     const closeButton = document.createElement('button');
-    closeButton.classList.add('close'); // Add class for the close button
-    closeButton.title = 'Close'; // Set title attribute
-    closeButton.innerHTML = '&times;'; // Set the close button content
+    closeButton.classList.add('close');
+    closeButton.title = 'Close';
+    closeButton.innerHTML = '&times;';
 
     header.appendChild(backButton);
     header.appendChild(flexSpace);
     header.appendChild(closeButton);
 
-    /*F0R WINDOW 2
-    const backButton = document.createElement('button');
-    backButton.classList.add('back'); // Add class for the back button
-    backButton.title = 'Back'; // Set title attribute
-    backButton.style.display = 'none'; // Initially hide the back button
-    backButton.innerHTML = '<i class="fa-solid fa-arrow-left" id="modal-return"></i>'; // Set the back button icon
-
-    const flexSpace = document.createElement('div');
-    flexSpace.classList.add('modal-flex-space'); // Add flexible space for header layout
-
-    const closeButton = document.createElement('button');
-    closeButton.classList.add('close'); // Add class for the close button
-    closeButton.title = 'Close'; // Set title attribute
-    closeButton.innerHTML = '&times;'; // Set the close button content
-
-    header.appendChild(backButton);
-    header.appendChild(flexSpace);
-    header.appendChild(closeButton);
-    */
     // CONTENT
     const galleryTitle = document.createElement('h1');
-    galleryTitle.id = 'gallery-edit-title'; // Set ID for the gallery title
-    galleryTitle.textContent = 'Gallerie photo'; // Set text content for the title
+    galleryTitle.id = 'gallery-edit-title';
+    galleryTitle.textContent = 'Gallerie photo';
+
     // GALLERY ROLL (adminGallery)
     const galleryRoll = document.createElement('div');
-    galleryRoll.classList.add('gallery-roll'); // Add class for the gallery roll
+    galleryRoll.classList.add('gallery-roll');
 
     content.appendChild(galleryTitle);
     content.appendChild(galleryRoll);
 
     const addPictureButton = document.createElement('button');
-    addPictureButton.id = 'add-picture-btn'; // Set ID for the add photo button
-    addPictureButton.textContent = 'Ajouter une photo'; // Set text content for the button
-    addPictureButton.title = 'Ajouter une photo'; // Set title attribute
+    addPictureButton.id = 'add-picture-btn';
+    addPictureButton.textContent = 'Ajouter une photo';
+    addPictureButton.title = 'Ajouter une photo';
 
     content.appendChild(addPictureButton);
 
-    // FOOTER (no footer)
+    // Call populateAdminGallery with works from utils.js
+    adminGallery(galleryRoll, works);
+
+    // Add close button functionality
+    closeButton.addEventListener('click', () => {
+        closeModal(document.querySelector('#edit-modal'));
+        console.log("Closing modal clicking X");
+    });
 
     // Return any elements for later access
     return { galleryRoll, addPictureButton, backButton, closeButton };
-
 }
 
-function populateAdminGallery(galleryRoll, works) {
+/**
+ * Populates the admin gallery (in MODAL WINDOW 1)
+ * 
+ * @param {HTMLElement} galleryRoll - The gallery container element.
+ * @param {Array} works - The array of works from utils.js.
+ */
+function adminGallery(galleryRoll, works) {
     if (!galleryRoll) {
         console.error("Gallery container not found.");
         return;
     }
 
-    // Clear existing content
     galleryRoll.innerHTML = "";
 
     // Populate the gallery with admin items
-    data.forEach((works) => {
+    works.forEach((item) => {
         const articleAdmin = document.createElement("article");
         articleAdmin.classList.add("article-admin");
         articleAdmin.setAttribute("data-category", item.category.name);
@@ -197,27 +180,69 @@ function populateAdminGallery(galleryRoll, works) {
         adminImg.src = item.imageUrl;
         adminImg.alt = item.title;
 
+        // Add a delete trash icon
         const deleteIcon = document.createElement('i');
         deleteIcon.classList.add('fa-solid', 'fa-trash', 'delete-icon');
         deleteIcon.title = 'Delete';
 
-        // Add event listener for deleting the item
-        deleteIcon.addEventListener('click', async () => {
-            const result = await httpDelete(works_endpoint, item.id, authToken);
-            if (result) {
-                showNotification('Photo deleted successfully', true);
-                // Remove the deleted item from the DOM
-                articleAdmin.remove();
-            } else {
-                showNotification('Error deleting the photo', false);
-            }
-        });
-
+        // Append the image and delete icon to the article
         articleAdmin.appendChild(adminImg);
         articleAdmin.appendChild(deleteIcon);
 
+        // Append the article to the gallery roll
         galleryRoll.appendChild(articleAdmin);
-        console.log("Added:", articleAdmin);
+
+        // Delete work if trash icon is clicked
+        deleteItem(deleteIcon, articleAdmin, item.id, authToken);
     });
 }
 
+/**
+ * DELETE ITEM: Handles the deletion of an item with a cancel option.
+ * In use in adminGallery
+ * 
+ * @param {HTMLElement} deleteIcon - The element that triggers the deletion.
+ * @param {HTMLElement} articleAdmin - The item to be deleted.
+ * @param {String} itemId - The ID of the item to delete.
+ * @param {String} authToken - The authentication.
+ */
+function deleteItem(deleteIcon, articleAdmin, itemId, authToken) {
+    deleteIcon.addEventListener('click', async (event) => {
+        event.preventDefault();
+
+        let cancelDeletion = false;
+        console.log("Asking for work deletion, waiting...");
+
+        const cancelNotification = document.createElement('div');
+        cancelNotification.classList.add('loading-indicator', 'error');
+        cancelNotification.innerHTML = `Suppression... <button id="cancelDelete">Annuler</button>`;
+        document.body.appendChild(cancelNotification);
+
+        const cancelButton = document.getElementById('cancelDelete');
+
+        cancelButton.addEventListener('click', () => {
+            cancelDeletion = true;
+            showNotification('Suppression annulée.', 'info');
+            console.log("Deletion cancelled by user");
+            cancelNotification.remove();
+        });
+
+        await new Promise((resolve) => setTimeout(resolve, 4300));
+
+        if (!cancelDeletion) {
+            const result = await httpDelete(works_endpoint, itemId, authToken);
+            cancelNotification.remove();
+
+            if (result) {
+                showNotification('Photo deleted successfully', 'info');
+                console.log("Deletion successful");
+                articleAdmin.remove();
+            } else {
+                showNotification('Error deleting the photo', 'error');
+                console.log("Work couldn't be deleted (backend error)");
+            }
+        } else {
+            cancelNotification.remove();
+        }
+    });
+}
